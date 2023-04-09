2d0d862db0b8b645ab8424ea7cb4bf0739c1bd2c
Mario Karajallo <mariokarajallo92@gmail.com>
2023-04-08 23:10:28 -0400

    feat: create function crearNuevoCliente()

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -34,6 +34,38 @@
 
       return;
     }
+
+    // crea un objeto con los datos del formulario
+    const cliente = {
+      nombre,
+      email,
+      telefono,
+      empresa,
+    };
+
+    cliente.id = Date.now();
+
+    crearNuevoCliente(cliente);
+  }
+
+  function crearNuevoCliente(cliente) {
+    const transaction = DB.transaction(["crm"], "readwrite");
+
+    const objectStore = transaction.objectStore("crm");
+
+    objectStore.add(cliente);
+
+    transaction.onerror = function () {
+      imprimirAlerta("hubo un error al crear el nuevo cliente", "error");
+    };
+
+    transaction.oncomplete = function () {
+      imprimirAlerta("Cliente agregado correctamente");
+
+      setTimeout(() => {
+        window.location.href = "index.html";
+      }, 3000);
+    };
   }
 
   function imprimirAlerta(mensaje, tipo) {
