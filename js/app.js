cbbedfb3df8772807954bfd7f63cb19a0ae93bd1
mariokarajallo <mariokarajallo92@gmail.com>
2023-04-12 14:51:13 -0400

    feat: add function eliminarRegistro()

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // creamos un IIFE (iffy pronunciacion), es como una burbuja que contiene todas las variables y funciones definidas dentro de ella y no permite que se filtren al ámbito global (global scope).
 (function () {
+  const listadoClientes = document.querySelector("#listado-clientes");
+
   // antes que nada cargamos todo el contenido primero
   document.addEventListener("DOMContentLoaded", () => {
     crearDB();
@@ -8,7 +10,38 @@
     if (window.indexedDB.open("crm", 1)) {
       obtenerClientes();
     }
+
+    listadoClientes.addEventListener("click", eliminarRegistro);
   });
+  //eliminar registro
+  function eliminarRegistro(e) {
+    if (e.target.classList.contains("eliminar")) {
+      //guardamos el id a eliminar y convertimos en tipo numero
+      const idEliminar = Number(e.target.dataset.cliente);
+      const nombreEliminar =
+        e.target.parentNode.parentNode.querySelector("p.nombre").innerText;
+
+      //consultamos si queremos eliminar o no
+      const confirmar = confirm(`Deseas eliminar al cliente ${nombreEliminar}`);
+
+      if (confirmar) {
+        const transaction = DB.transaction(["crm"], "readwrite");
+        const objectStore = transaction.objectStore("crm");
+
+        objectStore.delete(idEliminar);
+
+        transaction.oncomplete = function () {
+          console.log("eliminado");
+
+          e.target.parentElement.parentElement.remove();
+        };
+
+        transaction.onerror = function () {
+          console.log("hubo un error al eliminar");
+        };
+      }
+    }
+  }
 
   // Crea la base de datos indexedDB
   function crearDB() {
@@ -62,12 +95,10 @@
         if (cursor) {
           const { nombre, empresa, email, telefono, id } = cursor.value;
 
-          const listadoClientes = document.querySelector("#listado-clientes");
-
           listadoClientes.innerHTML += ` 
           <tr>
             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
+                <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold nombre"> ${nombre} </p>
                 <p class="text-sm leading-10 text-gray-700"> ${email} </p>
             </td>
             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 ">
@@ -78,7 +109,7 @@
             </td>
             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
             </td>
           </tr>  `;
 
