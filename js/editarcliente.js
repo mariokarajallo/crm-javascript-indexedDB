ccdcd285fdf9e5d596ed95ed8ecdaa7518d7f824
mariokarajallo <mariokarajallo92@gmail.com>
2023-04-11 20:57:50 -0400

    feat: add function conectarDB

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,4 +1,5 @@
 (function () {
+  let DB;
   document.addEventListener("DOMContentLoaded", () => {
     // verificar el ID de la URL
     const parametrosURL = new URLSearchParams(window.location.search);
@@ -9,4 +10,17 @@
       obtenerCliente(idCliente);
     }
   });
+
+  //conectamos a nuestra DB
+  function conectarDB() {
+    const abrirConexion = window.indexedDB.open("crm", 1);
+
+    abrirConexion.onerror = function () {
+      console.log("hubo un error al conectar con la base de datos");
+    };
+
+    abrirConexion.onsuccess = function () {
+      DB = abrirConexion.result;
+    };
+  }
 })();
