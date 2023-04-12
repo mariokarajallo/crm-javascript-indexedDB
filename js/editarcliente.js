13f9718f562048dc5ac6555028ac3416722d2e60
mariokarajallo <mariokarajallo92@gmail.com>
2023-04-11 21:11:33 -0400

    feat: add function obtenerCliente()

--- js/editarcliente.js
+++ js/editarcliente.js
@@ -1,16 +1,36 @@
 (function () {
   let DB;
   document.addEventListener("DOMContentLoaded", () => {
+    conectarDB();
     // verificar el ID de la URL
     const parametrosURL = new URLSearchParams(window.location.search);
 
     const idCliente = parametrosURL.get("id");
 
     if (idCliente) {
-      obtenerCliente(idCliente);
+      setTimeout(() => {
+        obtenerCliente(idCliente);
+      }, 100);
     }
   });
+  // comparar ID y traer los datos
+  function obtenerCliente(id) {
+    const transaction = DB.transaction(["crm"], "readwrite");
+    const objectStore = transaction.objectStore("crm");
 
+    const cliente = objectStore.openCursor();
+
+    cliente.onsuccess = function (e) {
+      const cursor = e.target.result;
+
+      if (cursor) {
+        if (cursor.value.id === Number(id)) {
+          llenarFormulario(cursor.value);
+        }
+        cursor.continue();
+      }
+    };
+  }
   //conectamos a nuestra DB
   function conectarDB() {
     const abrirConexion = window.indexedDB.open("crm", 1);
